49857dd0f9522989c99b816bde16fadf8087dc99
Israfil Arch <md.israfilarch@gmail.com>
2025-08-27 18:26:45 +0600

    Add files via upload

---
+++ Project-1.py
@@ -0,0 +1,20 @@
+# This is a python program which contant your information; (Project:1)
+
+
+# Code;
+
+my_information = {
+    "name": "Israfil",
+    "age" : 20,
+    "country": "Bangladesh",
+    "hobby": "coding",
+    "favorite_language": "Python",
+    "study": "Intermediate",
+    "Phone": "***********",
+    "email": "mdisrafilalom2023",
+    "address": "Dhaka, Bangladesh",
+}
+
+print(my_information)
+
+# You can change it with your information.
