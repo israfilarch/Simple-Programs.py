5886b1ff7ae80d39974b094ca64b8c08a5b29c32
Israfil Arch <md.israfilarch@gmail.com>
2025-08-29 21:39:42 +0600

    calculate the area of a rectangle

--- Projects-3.py
+++ Projects-3.py
@@ -1,8 +1,17 @@
 # Program to calculate the area of a rectangle
 
-a = float(input("Enter the length of the rectangle: "))   # takes rectangle length as float input
-b = float(input("Enter the breadth of the rectangle: "))  # takes rectangle breadth as float input
+while True:
+  a = float(input("Enter the length of the rectangle: "))   # takes rectangle length as float input
+  b = float(input("Enter the breadth of the rectangle: "))  # takes rectangle breadth as float input
 
-area = a * b   # calculates area by multiplying length and breadth
+  area = a * b   # calculates area by multiplying length and breadth
 
-print("The area of the rectangle is:", area)   # prints the calculated area
+
+  print("The area of the rectangle is:", area)   # prints the calculated area
+
+  again = str(input("Do you want again ,(yes/no): "))  # asks user if they want to run the program again
+  again.lower()   # converts user input to lowercase to handle case sensitivity
+  if again == 'no':   # if user input is  'no', break the loop and end the program
+        print("Thank you for using the program!") #Thank you for using the program
+        break
+  
