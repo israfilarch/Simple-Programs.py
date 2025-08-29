6ec7d2403a59d4979225146bc63c28cdfdfbb94f
Israfil Arch <md.israfilarch@gmail.com>
2025-08-29 21:01:54 +0600

    Calculate the area of a Rectangle

---
+++ Projects-3.py
@@ -0,0 +1,8 @@
+# Program to calculate the area of a rectangle
+
+a = float(input("Enter the length of the rectangle: "))   # takes rectangle length as float input
+b = float(input("Enter the breadth of the rectangle: "))  # takes rectangle breadth as float input
+
+area = a * b   # calculates area by multiplying length and breadth
+
+print("The area of the rectangle is:", area)   # prints the calculated area
